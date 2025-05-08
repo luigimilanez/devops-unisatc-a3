6cc67e0841feaf4ddd73327deb142d9b5314843d
Luigi <luigimcolonetti@gmail.com>
2025-05-08 20:41:22 -0300

    Atualização workflow V5.0

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    //keys: env.array('APP_KEYS'),
+    keys: ["ImJMpHjnCdJw4ii7jZzCXQ==","Jg239VoMach6Fh2LAH6ydA==","LAdmPTwE8oqyVjAV4pCkBQ==","f1gPGngKmE5xhyDktSpCVw=="]
   },
 });
